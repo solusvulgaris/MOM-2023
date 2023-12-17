c971412ad2ccd9f5ae5aefcf7d32aeae16fdf3a7
Alexandra Krauchanka <Alexandra_Krauchanka@epam.com>
2023-12-17 18:37:47 +0200

    1. Spring JMS basic connection

--- m3_ActiveMQ/Spring_Messaging_JMS_maven/src/main/java/com/ak/springboot/SpringbootApplication.java
+++ m3_ActiveMQ/Spring_Messaging_JMS_maven/src/main/java/com/ak/springboot/SpringbootApplication.java
@@ -2,9 +2,17 @@ package com.ak.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
 
+import javax.jms.ConnectionFactory;
+
+@EnableJms
 @SpringBootApplication
 public class SpringbootApplication {
 
@@ -13,6 +21,14 @@ public class SpringbootApplication {
 		ConfigurableApplicationContext context = SpringApplication.run(SpringbootApplication.class, args);
 		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
 
+		jmsTemplate.convertAndSend("order-queue", "Hello!");
 	}
 
+	@Bean
+	public JmsListenerContainerFactory warehouseFactory(ConnectionFactory factory, DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory containerFactory = new DefaultJmsListenerContainerFactory();
+		configurer.configure(containerFactory, factory);
+		return containerFactory;
+
+	}
 }
